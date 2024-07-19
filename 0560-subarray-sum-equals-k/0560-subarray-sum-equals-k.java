f05ec2c5d6085eec42dd23af46f51728a8b6553b
Karan Kumar <72157767+Tkr-Karan@users.noreply.github.com>
2024-07-19 21:42:05 +0530

    Time: 23 ms (91.63%), Space: 47.2 MB (6.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,34 +1,53 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
-        
-        // brute force
+        // optimising using Hashmap
         int n = nums.length;
         
-        int[] pf= new int[n];
-        
-        pf[0] = nums[0];
-        
-        for(int i = 1; i < n; i++){
-            pf[i] = pf[i-1] + nums[i];
-        }
-        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
         
         int cnt = 0;
+        
         int sum = 0;
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(i == 0){
-                    sum = pf[j];
+            sum += nums[i];
+            
+            if(hm.containsKey(sum - k)){
+                cnt += hm.get(sum-k);
+            }
+            hm.put(sum, hm.getOrDefault(sum, 0)+1);
+        }
+        
+        
+        
+        // brute force
+        // int n = nums.length;
+        
+//         int[] pf= new int[n];
+        
+//         pf[0] = nums[0];
+        
+//         for(int i = 1; i < n; i++){
+//             pf[i] = pf[i-1] + nums[i];
+//         }
+        
+        
+//         int cnt = 0;
+//         int sum = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i; j < n; j++){
+//                 if(i == 0){
+//                     sum = pf[j];
                    
-                }else{
-                    sum = pf[j] - pf[i-1];
-                }
+//                 }else{
+//                     sum = pf[j] - pf[i-1];
+//                 }
                 
            
-                 if(sum == k) cnt++;
-            }
-        }
+//                  if(sum == k) cnt++;
+//             }
+//         }
         
         return cnt;
     }
