10523d4a63592099110c3fec1f2fdfdc150c94c1
Karan Kumar <72157767+Tkr-Karan@users.noreply.github.com>
2024-07-20 19:11:24 +0530

    Time: 1 ms (74.56%), Space: 42.2 MB (9.34%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,32 +2,49 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         int n = strs.length;
         
-//         Method 2;
+        Arrays.sort(strs);  // TC :O(N*logN)
         
-        int minLen = Integer.MAX_VALUE;
+        String first = strs[0];
+        String second = strs[n-1];
         
-        for(int i = 0; i < n; i++){
-            minLen = Math.min(minLen, strs[i].length());
-        }
+        int indx = 0;
         
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < minLen; i++){
-            boolean isCommon = true;
-            
-            for(int j = 0; j < n; j++){
-                if(strs[j].charAt(i) != strs[0].charAt(i)){
-                    isCommon = false;
-                    break;
-                }
-            }
-            
-            // checking if we found the common or not i
-            // if we found add in res other break the loop and print the result
-            if(isCommon == true) res = res.append(strs[0].charAt(i));
+        while(indx < first.length() && indx < second.length()){
+            if(first.charAt(indx) == second.charAt(indx)) indx++;
             else break;
         }
         
-        return res.toString();
+        
+        return first.substring(0, indx);
+        
+        
+        
+//         Method 2;
+        
+//         int minLen = Integer.MAX_VALUE;
+        
+//         for(int i = 0; i < n; i++){
+//             minLen = Math.min(minLen, strs[i].length());
+//         }
+        
+//         StringBuilder res = new StringBuilder();
+//         for(int i = 0; i < minLen; i++){
+//             boolean isCommon = true;
+            
+//             for(int j = 0; j < n; j++){
+//                 if(strs[j].charAt(i) != strs[0].charAt(i)){
+//                     isCommon = false;
+//                     break;
+//                 }
+//             }
+            
+//             // checking if we found the common or not i
+//             // if we found add in res other break the loop and print the result
+//             if(isCommon == true) res = res.append(strs[0].charAt(i));
+//             else break;
+//         }
+        
+//         return res.toString();
         
         
         // Brute Force
