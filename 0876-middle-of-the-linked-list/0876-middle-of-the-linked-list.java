b6dee3d27a6c29f4842d72b8fb16d72216ea2564
Karan Kumar <72157767+Tkr-Karan@users.noreply.github.com>
2024-07-24 00:14:58 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (5.26%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -15,25 +15,35 @@ class Solution {
         
         if(head == null) return head;
         
-        
-        ListNode curr = head;
-        int count = 0;
-        while(curr.next != null){
-            count++;
-            curr = curr.next;
-        }
+        // Approach 2
         
         while(fast != null && fast.next != null){
-            if(count % 2 == 0){
-                head = slow.next;
-            }else{
-                head = slow.next;
-            }
-            
             slow = slow.next;
             fast = fast.next.next;
         }
         
-        return head;
+        return slow;
+        
+        
+        // Approac 1
+//         ListNode curr = head;
+//         int count = 0;
+//         while(curr.next != null){
+//             count++;
+//             curr = curr.next;
+//         }
+        
+//         while(fast != null && fast.next != null){
+//             if(count % 2 == 0){
+//                 head = slow.next;
+//             }else{
+//                 head = slow.next;
+//             }
+            
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+        
+//         return head;
     }
 }
